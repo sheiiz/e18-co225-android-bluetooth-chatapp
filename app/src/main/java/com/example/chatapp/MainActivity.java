8fc80a5d5408372a430c659426cd49680f945562
Sanduni Nirasha <e18329@eng.pdn.ac.lk>
2022-02-16 01:37:04 +0530

    3 Tabs

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabAccessAdapter myTabAccessAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar=findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("ChatApp");
+
+        myViewPager=findViewById(R.id.main_tabs_pager);
+        myTabAccessAdapter= new TabAccessAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessAdapter);
+
+        myTabLayout=findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
