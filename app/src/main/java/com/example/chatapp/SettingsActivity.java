8a21f94d1951ed8f9de97e7230370f7443536cd4
Avishka Abeywickrama <avishkaabeywickrama99@gmail.com>
2022-02-20 11:40:05 +0530

    Created settings activity xml file

---
+++ app/src/main/java/com/example/chatapp/SettingsActivity.java
@@ -0,0 +1,42 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+/*
+        package com.example.chatapp;
+
+import android.widget.EditText;
+
+    public class SettingsActivity {
+
+        //Controls
+        private Button updateAccountSettings;
+        private EditText userName, userStatus;
+        private CircleImageView userProfileImage;
+
+        //Firebase
+        private FirebaseAuth mAuth;
+        private DatabaseReference dataPath;
+        private StorageReference UserProfileImagePath;
+        private StorageTask loadingTask;
+
+        private String currentUserId;
+
+
+
+    }
+
+ */
+}
+
