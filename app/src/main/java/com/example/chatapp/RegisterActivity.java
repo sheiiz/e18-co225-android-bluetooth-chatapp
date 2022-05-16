57cb935ea237ccafda961b18e4a99306a620dbbe
Avishka Abeywickrama <avishkaabeywickrama99@gmail.com>
2022-05-16 16:51:36 +0530

    added image selection

--- app/src/main/java/com/example/chatapp/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/RegisterActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,14 +30,29 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText UserEmail,UserPassword;
     private TextView AlreadyHaveAccountLink;
 
+
     private ProgressDialog loadingBar;
     private DatabaseReference RootReference;
     Database skyChatDB;
+
+    public RegisterActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ImageButton gallery = findViewById(R.id.set_profile_image);
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+
 
         skyChatDB= new Database(this);
 
@@ -58,6 +77,16 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && data!=null) {
+            Uri selectedImage = data.getData();
+            ImageView set_profile_image = findViewById(R.id.set_profile_image);
+            set_profile_image.setImageURI(selectedImage);
+        }
     }
 
     private void createAccount(){
